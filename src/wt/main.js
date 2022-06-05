3953c5dca1a895d9fa1703220d592dc1ebdcf956
Max Andreev <Maksim_Andreyeu@epam.com>
2022-06-05 09:26:41 +0300

    feat: implement workers threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,26 @@
+import { cpus } from "os";
+import { Worker, isMainThread } from "worker_threads";
+
+const CPUS = cpus();
+
 export const performCalculations = async () => {
-    // Write your code here
+  if (isMainThread) {
+    const workers = CPUS.map((cpu, index) => {
+      return runWorker("./worker.js", 10 + index);
+    });
+
+    return await Promise.all(workers.map((worker) => worker));
+  }
 };
+
+function runWorker(path, workerData) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(path, { workerData });
+    worker.on("message", resolve);
+    worker.on("error", reject);
+  });
+}
+
+performCalculations().then((result) => {
+  console.log(result);
+});
