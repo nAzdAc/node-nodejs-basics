5dacb127c5b6696bda3298022e896e6001dce4cb
Max Andreev <Maksim_Andreyeu@epam.com>
2022-06-05 11:16:29 +0300

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,31 @@
+import { fork } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "./files/script.js");
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(filePath, args);
+
+  child.on("message", (childMessage) => {
+    // console.log("Child Message: ", childMessage);
+    if (childMessage) {
+      process.stdout.write(Buffer.from(childMessage.data));
+    } else {
+      process.exit();
+    }
+  });
+
+  child.on("error", (err) => {
+    console.log("Something went wrong: ", err);
+  });
+
+  process.stdin.on("data", (chunk) => {
+    // console.log("Chunk for child: ", chunk);
+    child.send(chunk);
+  });
 };
+
+spawnChildProcess([1, 2]);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,12 +1,21 @@
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-console.log(`Total number of arguments is ${arguments.length}`);
-console.log(`Arguments: ${JSON.stringify(arguments)}`);
+console.log(`Total number of arguments is ${args.length}`);
+console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    if (chunkStringified.includes('CLOSE')) process.exit(0);
-    process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
+  //   console.log("Chunk from master: ", chunk);
+  const chunkStringified = Buffer.from(chunk.data).toString();
+  if (chunkStringified.includes("CLOSE")) {
+    process.send(null);
+  } else {
+    process.send(chunk);
+  }
+  //   process.stdout.write(`Received from master process: ${chunk.toString()}\n`);
 };
 
-process.stdin.on('data', echoInput);
+// process.stdin.on("data", echoInput);
+process.on("message", (masterMessage) => {
+  //   console.log("MASTER Message: ", masterMessage);
+  echoInput(masterMessage);
+});
